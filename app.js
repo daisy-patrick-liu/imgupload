5bd2966fda4dcf69324d0de189acc512df7aaac3
刘梦 <954654261@qq.com>
2017-11-27 18:24:22 +0800

    completed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var formidable = require('formidable');
+var fs = require("fs");
 var app = express();
 
 app.get('/', function(req, res) {
@@ -7,6 +9,16 @@ app.get('/', function(req, res) {
 
 // public资源目录
 app.use('/public', express.static('public'));
+
+// 处理post请求
+app.post('/upload', function(req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, file) {
+    fs.writeFileSync('public/test.png', fs.readFileSync(file.upload.path));
+    res.redirect('/public/upload.html');
+  })
+  
+})
 
 var server = app.listen(8000, function() {
   let host = server.address().address;
